f22ffaba5692dc94b081e223899163ccfb784b28
Celestin Niyonsaba <niyoceles3@gmail.com>
2021-04-26 10:35:13 +0200

    datasource with default schema

--- src/main/java/com/niyonsaba/pma/security/SecurityConfiguration.java
+++ src/main/java/com/niyonsaba/pma/security/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.niyonsaba.pma.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,9 +15,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.withDefaultSchema()
 				.withUser("myuser").password("pass").roles("USER")
 				.and()
 				.withUser("celes").password("pass2").roles("USER")
@@ -33,8 +41,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 				.antMatchers("/projects/new").hasRole("ADMIN")
 				.antMatchers("/employees/new").hasRole("ADMIN")
+				.antMatchers("/h2_console/**").permitAll()
 				.antMatchers("/").authenticated().and().formLogin();
-		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 	}
 	
 }
