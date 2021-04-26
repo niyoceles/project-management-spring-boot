b91ff268eddd6d867bc0e887d2c030d893cfe8a2
Celestin Niyonsaba <niyoceles3@gmail.com>
2021-04-26 08:33:24 +0200

    use logging Around notation

--- src/main/java/com/niyonsaba/pma/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/niyonsaba/pma/logging/ApplicationLoggerAspect.java
@@ -1,7 +1,12 @@
 package com.niyonsaba.pma.logging;
 
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -21,13 +26,53 @@ public class ApplicationLoggerAspect {
 		//Empty method just to name the location specified in the pointcut
 	}
 	
-	@After("definePackagePointcuts()")
-	public void log() {
-		log.debug("_________________________________");
+//	@Before("definePackagePointcuts()")
+//	public void logBefore(JoinPoint jp) {
+//		log.debug(" \n \n \n ");
+//		log.debug("********** Before Method execution ***** \n {}.{} () with arguments[s] = {}",
+//				jp.getSignature().getDeclaringTypeName(),
+//				jp.getSignature().getName(),
+//				Arrays.toString(jp.getArgs()));
+//		log.debug("_________________________________ \n \n \n ");
+//	}
+//	
+//	@After("definePackagePointcuts()")
+//	public void logAfter(JoinPoint jp) {
+//		log.debug(" \n \n \n ");
+//		log.debug("********** After Method execution ***** \n {}.{} () with arguments[s] = {}",
+//				jp.getSignature().getDeclaringTypeName(),
+//				jp.getSignature().getName(),
+//				Arrays.toString(jp.getArgs()));
+//		log.debug("_________________________________ \n \n \n ");
+//	}
+	
+	@Around("definePackagePointcuts()")
+	public Object logAround(ProceedingJoinPoint jp) {
+		log.debug(" \n \n \n ");
+		log.debug("********** Before Method execution ***** \n {}.{} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(),
+				Arrays.toString(jp.getArgs()));
+		log.debug("_________________________________ \n \n \n ");
+		
+		Object o= null;
+		try {
+			o = jp.proceed();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		log.debug(" \n \n \n ");
+		log.debug("********** After Method execution ***** \n {}.{} () with arguments[s] = {}",
+				jp.getSignature().getDeclaringTypeName(),
+				jp.getSignature().getName(),
+				Arrays.toString(jp.getArgs()));
+		log.debug("_________________________________ \n \n \n ");
+		
+		return o;
+		
 	}
 	
-	@Before("definePackagePointcuts()")
-	public void logBefore() {
-		log.debug("_________________________________");
-	}
+	
 }
